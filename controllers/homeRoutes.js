2949e9ea0d6d6fde07fb49bcdd294ca35f52bfd1
Luc Burns <burnsluc2@gmail.com>
2021-04-30 12:09:13 -0400

    login working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,4 +41,22 @@ router.get('/signup/employer', (req, res) => {
     }
 })
 
+router.get('/logout', async (req, res) => {
+    try {
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  
+
 module.exports = router;
