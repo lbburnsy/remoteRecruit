f845969912fd4a7deb18750601d59b4a38cfd5fa
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-05-02 18:50:16 -0400

    fixed the bug where the categories were displayed as an array, i added a condition in the helpers file and updated the handlebars file related

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,17 @@
+//const { Json } = require("sequelize/types/lib/utils");
+
 module.exports = {
     format_date: (date) => {
         // Format date as MM/DD/YYYY
         return date.toLocaleDateString();
+      },
+
+      format_categories: (categories) => {
+        const Categoriesobj = JSON.parse(categories);
+        // Format date as MM/DD/YYYY
+       return Categoriesobj.map((element, index) => (index === Categoriesobj.length - 1) ? ` ${element}` : ` ${element}`);
+      // return Categoriesobj.map((element, index) => `<li> ${element} </li>` );
+      
+
       }
 }
