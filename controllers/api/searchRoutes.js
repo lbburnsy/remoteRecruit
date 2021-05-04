0dfc9dee78ea53065b7db8fac48f5a79a0a78072
Brittany Plummer <brittanyplummer@gmail.com>
2021-05-04 12:12:20 -0400

    worked on implementing the ini search new tech feature, finished the searchRoutes.js and added the searchbar to navbar.handlebars. as well as created a renderResults.handlebars

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -9,31 +9,126 @@ const {
 } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-
+const searchData = [
+    {
+        "name": "Back End Developer",
+        "description": "Full stack developer position with focus Python",
+        "qualifications": "3+ years professional experience",
+        "location": "USA",
+        "website": "wikipedia.com",
+        "category": "backend"
+    },
+    {
+        "name": "Back End Developer",
+        "description": "Full stack developer position with focus Ruby",
+        "qualifications": "5+ years professional experience",
+        "location": "New York",
+        "website": "wikipedia.com",
+        "category": "backend"
+    },
+    {
+        "name": "Back End Developer",
+        "description": "Full stack developer position with focus Python",
+        "qualifications": "1+ years professional experience",
+        "location": "Remote",
+        "website": "wikipedia.com",
+        "category": "backend"
+    },
+    {
+        "name": "Back End Developer",
+        "description": "Full stack developer position with focus Node",
+        "qualifications": "2+ years professional experience",
+        "location": "North America",
+        "website": "wikipedia.com",
+        "category": "backend"
+    },
+    {
+        "name": "Front End Developer",
+        "description": "Full stack developer position with focus Javascript",
+        "qualifications": "3+ years professional experience",
+        "location": "USA",
+        "website": "wikipedia.com",
+        "category": "frontend"
+    },
+    {
+        "name": "Front End Developer",
+        "description": "Full stack developer position with focus Javascript",
+        "qualifications": "5+ years professional experience",
+        "location": "New York",
+        "website": "wikipedia.com",
+        "category": "frontend"
+    },
+    {
+        "name": "Front End Developer",
+        "description": "Full stack developer position with focus Javascript",
+        "qualifications": "1+ years professional experience",
+        "location": "Remote",
+        "website": "wikipedia.com",
+        "category": "frontend"
+    },
+    {
+        "name": "Front End Developer",
+        "description": "Full stack developer position with focus Javascript",
+        "qualifications": "2+ years professional experience",
+        "location": "North America",
+        "website": "wikipedia.com",
+        "category": "frontend"
+    },
+    {
+        "name": "Full Stack Developer",
+        "description": "Full stack developer position with focus Node and Javascript",
+        "qualifications": "3+ years professional experience",
+        "location": "USA",
+        "website": "wikipedia.com",
+        "category": "fullstack"
+    },
+    {
+        "name": "Full Stack Developer",
+        "description": "Full stack developer position with focus Node and Javascript",
+        "qualifications": "5+ years professional experience",
+        "location": "New York",
+        "website": "wikipedia.com",
+        "category": "fullstack"
+    },
+    {
+        "name": "Full Stack Developer",
+        "description": "Full stack developer position with focus Node and Javascript",
+        "qualifications": "1+ years professional experience",
+        "location": "Remote",
+        "website": "wikipedia.com",
+        "category": "fullstack"
+    },
+    {
+        "name": "Full Stack Developer",
+        "description": "Full stack developer position with focus Node and Javascript",
+        "qualifications": "2+ years professional experience",
+        "location": "North America",
+        "website": "wikipedia.com",
+        "category": "fullstack"
+    }
+]
 
 router.get("/:searchTerm", async (req, res) => {
     const searchTerm = req.params.searchTerm;
     const documents = await FrontEnd.findAll();
     
     res.status(200).send();
-
+    
     let miniSearch = new MiniSearch({
-        storeFields: ['title', 'category'], 
-        fields: ['title', 'text'],
+        storeFields: ['name', 'description', 'qualifications', 'location', 'website', 'category'], 
+        fields: ['name', 'description', 'qualifications', 'location', 'website', 'category']
     });
 
-    miniSearch.addAll(documents);
+    miniSearch.addAll(searchData);;
     let results = miniSearch.search(searchTerm);
-    // gets the request and comment it back
-    res.json(results)
-    // replace ^ res.render("searchResults(handelbar template", object with results)
-    //res.render('searchResults{{handelbars}}, object);
+   
+    res.render('searchResults', results);
 
-    // object = []
+   
 });
 
 module.exports = router;
 
-//make handelbar first
+
 
 
