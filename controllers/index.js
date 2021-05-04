933606cd9ee6e7b30b108d82bd423c589819748e
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-05-04 16:31:13 -0400

    minisearch functionality working

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
 const homeRoutes = require("./homeRoutes");
+const searchRoutes = require("./searchRoutes");
 const apiRoutes = require("./api");
 const profileRoutes = require("./profileRoutes");
 
 
 router.use("/", homeRoutes);
+router.use("/search", searchRoutes);
 router.use("/api", apiRoutes);
 router.use("/profile", profileRoutes);
 
---
+++ public/js/navBar.js
@@ -0,0 +1,12 @@
+var clearBtn = document.querySelector("#search-btn");
+clearBtn.addEventListener("click", clearSearch);
+
+function clearSearch (){
+   var textSearch=document.getElementById('searchbar');
+   var txtrsults=textSearch.value;
+   window.location = '/search/'+txtrsults;
+  }
+
+
+
+  
