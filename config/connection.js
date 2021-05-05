adaa50e91c3463b87e4737684a1280c696dbfd3b
Luc Burns <burnsluc2@gmail.com>
2021-05-05 13:52:04 -0400

    config folder commented and formatted

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 let sequelize;
 
+// Functionality to work with JawsDB on heroku
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
